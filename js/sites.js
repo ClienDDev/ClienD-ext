3257074addf943028c9989b04a07d77145c2c9ea
atnartur <atnartur@yandex.ru>
2014-02-08 23:55:27 +0400

    Added panel

--- js/sites.js
+++ js/sites.js
@@ -2,6 +2,10 @@ cliend_ext = {}
 
 cliend_ext.edutatar = {}
 
+
+/**
+ * Инициализация стилей
+*/
 cliend_ext.edutatar.styles_init = function() {
     jQuery('head').append('<link rel="stylesheet" href="'+chrome.extension.getURL('/styles/fonts.css')+'">');
     if (document.location.hash.indexOf('cliend_debug') === 1) {
@@ -14,24 +18,51 @@ cliend_ext.edutatar.styles_init = function() {
     }
 }
 
+
+/**
+ * Инициализация HTML шаблона
+*/
 cliend_ext.edutatar.html_init = function(){
     jQuery('body').append('<div id="cliend">'+
-                                '<div class="cliend_left">'+
-                                    '<h1>ClienD</h1>'+
-                                '</div>'+
-                                '<div class="cliend_center>'+
-                                '</div>'+
-                                '<div class="cliend_right>'+
-                                    ''
-                                '</div>'+
+                                '<h1>ClienD</h1>'+
+                                '<p class="cliend_message" id="cliend_message"></p>'+
                         '</div>');
 }
+
+
+/**
+ * Изменение сообщения
+ *
+ * @param {string} text Текст сообщения
+*/
+cliend_ext.edutatar.msg = function(text){
+    jQuery('#cliend_message').text(text);    
+}
+
+
+cliend_ext.edutatar.page = {}
+
+
+/**
+ * Инициализация функций на разных страницах
+*/
+cliend_ext.edutatar.page.init = function(){
+    
+}
+
+
+
+
+
+
+
 
 
 jQuery(document).ready(function(){
     if (document.location.href.indexOf('edu.tatar.ru') > 1) {
         cliend_ext.edutatar.styles_init();
-        cliend_ext.edutatar.html_init(); 
+        cliend_ext.edutatar.html_init();
+        cliend_ext.edutatar.msg('хорошей работы!');
     }
     else if (document.location.href.indexOf('cliend.ru') > 1) {
         
