4141b4e98a170483658ea68a46948c8fad947f41
atnartur <i@atnartur.ru>
2016-02-08 22:57:32 +0300

    ! не загружается jQuery для постинга из ВК

--- js/vk/extension.js
+++ js/vk/extension.js
@@ -4,7 +4,7 @@
  */
 
 $(document).ready(function(){
-    cliend_include_js(chrome.extension.getURL("js/libs/jquery.min.js"));
+    cliend_include_js(chrome.extension.getURL("libs/jquery/dist/jquery.min.js"));
     cliend_include_js(chrome.extension.getURL("js/common/get_vk_post_id.js"));
     cliend_include_js(chrome.extension.getURL("js/vk/post_share.js"));
     $.get(chrome.extension.getURL("js/vk/template.html"), function(res){
