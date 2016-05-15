abead5c58e7e8607ac894b00f5061fb0cfdd958d
atnartur <i@atnartur.ru>
2016-05-15 21:51:37 +0300

    + Автовыбор урока, на который было задано домашнее задание

--- js/fixtures/school/journal/lesson-edit.js
+++ js/fixtures/school/journal/lesson-edit.js
@@ -1,3 +1,11 @@
 /*
 * @Author: Artur Atnagulov (ClienDDev team)
 */
+
+$(document).ready(function(){
+	var $select = $('#lesson_homework_lesson_id');
+	if($select.val() == '0'){ // если дата домашки пустая
+		$select.find('option').removeAttr('selected');
+		$select.find('option:eq(1)').prop('selected', true); // устанавливаем первую дату 
+	}
+});
