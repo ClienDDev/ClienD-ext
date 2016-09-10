60d0aa3186da492211e7d063799fab040edd2e4d
atnartur <i@atnartur.ru>
2016-09-10 20:43:34 +0300

    ! кнопка публикации на edu.tatar.ru из ВК

--- js/vk/post_share.js
+++ js/vk/post_share.js
@@ -7,17 +7,17 @@
 	$ = jQuery;
 
 	function start(){
-		$('div.post_share, a.reply_link').unbind('click').click(function(){
+		$('a.post_share').unbind('click').click(function(){
 			console.log('cliend: click')
 			
 			var post_href;
 
-			if(location.href.indexOf('new.vk.com') !== -1) // для нового интерфейса вк
-				post_href = $(this).closest('.post.page_block').find('.post_header .post_date a.post_link').attr('href');
+			// if(location.href.indexOf('new.vk.com') !== -1) // для нового интерфейса вк
 			if(location.href.indexOf('vk.com/wall') !== -1) // для страницы записи (пример: https://vk.com/wall-413099_1965)
 				post_href = location.href;
 			else // для остальных случаев (стена группы)
-				post_href = $(this).closest('.post_info').find('.replies small a').attr('href');
+				post_href = $(this).closest('.post.page_block').find('.post_header .post_date a.post_link').attr('href');
+				// post_href = $(this).closest('.post_info').find('.replies small a').attr('href');
 			
 			console.log(post_href);
 			var post_id = cliend_get_vk_post_id_from_url(post_href);
