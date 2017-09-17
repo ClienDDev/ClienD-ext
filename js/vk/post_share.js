f90a3d05751791104458b4315ffc1e9bdc4e9d82
atnartur <i@atnartur.ru>
2017-09-17 16:11:31 +0300

    ! jquery not found in vk

--- js/vk/post_share.js
+++ js/vk/post_share.js
@@ -3,46 +3,54 @@
  * Developer: Artur Atnagulov (atnartur)
  */
 
-(function(){
-	$ = jQuery;
+// Anonymous "self-invoking" function
+(function() {
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://yastatic.net/jquery/3.1.1/jquery.min.js';
+    script.type = 'text/javascript';
+    script.onload = function() {
+        var $ = window.jQuery;
+        
+        function start(){
+			$('a.post_share').unbind('click').click(function(){
+				console.log('cliend: click')
+				
+				var post_href;
 
-	function start(){
-		$('a.post_share').unbind('click').click(function(){
-			console.log('cliend: click')
-			
-			var post_href;
-
-			// if(location.href.indexOf('new.vk.com') !== -1) // для нового интерфейса вк
-			if(location.href.indexOf('vk.com/wall') !== -1) // для страницы записи (пример: https://vk.com/wall-413099_1965)
-				post_href = location.href;
-			else // для остальных случаев (стена группы)
-				post_href = $(this).closest('.post').find('.post_header .post_date a.post_link').attr('href');
-				// post_href = $(this).closest('.post_info').find('.replies small a').attr('href');
-			
-			console.log(post_href);
-			var post_id = cliend_get_vk_post_id_from_url(post_href);
-			console.log(post_id);
-			
-			var interval = setInterval(function(){
-				var $container = $('.like_share_cont:visible')
-				if($container.length == 1){
-					clearInterval(interval);
-					
-					$('#like_share_send:visible').after($('#cliend_share').html());
-					
-					$container.find('#cliend_share_button').click(function(){
-						window.open('https://edu.tatar.ru/admin/page/news_block#vk_post_id=' + post_id);
-						$('#box_layer').click(); // закрываем окно
-					});
-				}
-			}, 100);    
-		});
-	}
-	
-	$(document).ready(function(){
-		start();
-		setInterval(function(){
+				// if(location.href.indexOf('new.vk.com') !== -1) // для нового интерфейса вк
+				if(location.href.indexOf('vk.com/wall') !== -1) // для страницы записи (пример: https://vk.com/wall-413099_1965)
+					post_href = location.href;
+				else // для остальных случаев (стена группы)
+					post_href = $(this).closest('.post').find('.post_header .post_date a.post_link').attr('href');
+					// post_href = $(this).closest('.post_info').find('.replies small a').attr('href');
+				
+				console.log(post_href);
+				var post_id = cliend_get_vk_post_id_from_url(post_href);
+				console.log(post_id);
+				
+				var interval = setInterval(function(){
+					var $container = $('.like_share_cont:visible')
+					if($container.length == 1){
+						clearInterval(interval);
+						
+						$('#like_share_send:visible').after($('#cliend_share').html());
+						
+						$container.find('#cliend_share_button').click(function(){
+							window.open('https://edu.tatar.ru/admin/page/news_block#vk_post_id=' + post_id);
+							$('#box_layer').click(); // закрываем окно
+						});
+					}
+				}, 100);    
+			});
+		}
+		
+		$(document).ready(function(){
 			start();
-		}, 1000)
-	});
+			setInterval(function(){
+				start();
+			}, 1000)
+		});
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
 })();
