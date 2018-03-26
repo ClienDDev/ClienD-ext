1836eed4c5e7f4c933b173b0c1a419762935d4ea
atnartur <i@atnartur.ru>
2018-03-27 00:57:03 +0300

    ! vk post page cleanup

--- js/fixtures/admin/page/news/edit/page.js
+++ js/fixtures/admin/page/news/edit/page.js
@@ -1,56 +1,6 @@
-function php_trim(str, charlist) {
-    //  discuss at: http://phpjs.org/functions/trim/
-    // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // improved by: mdsjack (http://www.mdsjack.bo.it)
-    // improved by: Alexander Ermolaev (http://snippets.dzone.com/user/AlexanderErmolaev)
-    // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // improved by: Steven Levithan (http://blog.stevenlevithan.com)
-    // improved by: Jack
-    //    input by: Erkekjetter
-    //    input by: DxGx
-    // bugfixed by: Onno Marsman
-    //   example 1: trim('    Kevin van Zonneveld    ');
-    //   returns 1: 'Kevin van Zonneveld'
-    //   example 2: trim('Hello World', 'Hdle');
-    //   returns 2: 'o Wor'
-    //   example 3: trim(16, 1);
-    //   returns 3: 6
-
-    var whitespace, l = 0,
-        i = 0;
-    str += '';
-
-    if (!charlist) {
-        // default list
-        whitespace =
-            ' \n\r\t\f\x0b\xa0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000';
-    } else {
-        // preg_quote custom list
-        charlist += '';
-        whitespace = charlist.replace(/([\[\]\(\)\.\?\/\*\{\}\+\$\^\:])/g, '$1');
-    }
-
-    l = str.length;
-    for (i = 0; i < l; i++) {
-        if (whitespace.indexOf(str.charAt(i)) === -1) {
-            str = str.substring(i);
-            break;
-        }
-    }
-
-    l = str.length;
-    for (i = l - 1; i >= 0; i--) {
-        if (whitespace.indexOf(str.charAt(i)) === -1) {
-            str = str.substring(0, i + 1);
-            break;
-        }
-    }
-
-    return whitespace.indexOf(str.charAt(0)) === -1 ? str : '';
-}
-
 (function($) {
     const EOL = '\n';
+
     function vk_post(post_id) {
         $.ajax({
             url: "https://api.vk.com/method/wall.getById?" + $.param({
@@ -133,23 +83,26 @@ function php_trim(str, charlist) {
     $(document).ready(function() {
         $('#content h2').before(
             '<div class="cliend ptop pbottom">' +
-            '<button id="cliend_vk_post_import" class="btn btn-primary pull-right">Добавить пост из ВКонтакте</button>' +
-            '</div>');
+                '<button id="cliend_vk_post_import" class="btn btn-primary pull-right">' +
+                    'Добавить пост из ВКонтакте' +
+                '</button>' +
+            '</div>'
+        );
 
-        var vk_post_id = $('body').data('vk_post_id');
+        let vk_post_id = $('body').data('vk_post_id');
 
         if (typeof vk_post_id !== 'undefined')
             vk_post(vk_post_id);
 
         $('#cliend_vk_post_import').click(function() {
-            var url = prompt('Введите URL поста ВКонтакте');
+            let url = prompt('Введите URL поста ВКонтакте');
 
-            if (url == '' && url == null) {
+            if (url === '' || url === null) {
                 alert('Не введен URL поста');
                 return false;
             }
 
-            var post_id = cliend_get_vk_post_id_from_url(url);
+            let post_id = cliend_get_vk_post_id_from_url(url);
 
             if (post_id === false) {
                 alert('Ошибка при разборе адреса. Возможно URL неправильный');
@@ -162,5 +115,4 @@ function php_trim(str, charlist) {
         // Выделение галочек "Район" и "Республика"
         $('#news_trans_region, #news_trans_global').click();
     })
-
-})(jQuery)
+})(jQuery);
